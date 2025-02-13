bba2229484f25bd084197c9984c2b9a5606ce1f9
Emile Belot <emile.belot@gmail.com>
2025-02-13 09:33:43 +0100

    change the way we add mat capacity

--- Planification/main.py
+++ Planification/main.py
@@ -10,14 +10,14 @@ from bellman import main_bellman
 warehouses_config, category_mapping = load_config()
 
 # Sélectionner un entrepôt
-warehouse_name = "three_level_line_warehouse"
+warehouse_name = "one_level_F_warehouse"
 
 # Build warehouse
 warehouse_3d = build_warehouse(warehouse_name, warehouses_config)
 
 #False by default. If True, will display the warehouse in a plot
-warehouse_3d.display()
-warehouse_3d.show_graph()
+warehouse_3d.display(True)
+warehouse_3d.show_graph(True)
 
 #Generates the adjacency matrix
 final_adjacency_matrix, coordinate_to_index = main_adjacency(warehouse_3d, category_mapping)
